4ced1d18ca2089ee097fee1271ada9172df100ee
James Corbould <james.bron@hotmail.com>
2014-07-23 07:36:32 +1200

    Added FeedHistory and FeedItemHistory classes.

---
+++ jarwin.DAL/FeedHistory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jarwin.DAL
+{
+    [Table(Name="feed_history")]
+    public class FeedHistory
+    {
+        [Column(Name="feed_id", IsPrimaryKey=true)] public int feedID { get; set; }
+        [Column(Name="feed_uri")] public string feedURI { get; set; }
+        [Column(Name="title")] public string title { get; set; }
+        [Column(Name="description")] public string description { get; set; }
+        [Column(Name="last_build_datetime")] public DateTime lastBuildDateTime { get; set; }
+        [Column(Name="last_download_datetime", IsPrimaryKey=true)] public DateTime lastDownloadDateTime { get; set; } 
+        [Column(Name="language")] public string language { get; set; }
+        [Column(Name="update_frequency")] public int? updateFrequency { get; set; }
+        [Column(Name="update_period")] public string updatePeriod { get; set; }
+        [Column(Name="site_uri")] public string siteURI { get; set; }
+        [Column(Name="status")] public string status { get; set; }
+        [Column(Name="type")] public string type { get; set; }
+        [Column(Name="folder_id")] public int? folderID { get; set; }
+    }
+}
---
+++ jarwin.DAL/FeedItemHistory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jarwin.DAL
+{
+    [Table(Name="feed_item_history")]
+    public class FeedItemHistory
+    {
+        [Column(Name="feed_id", IsPrimaryKey=true)] public int feedID { get; set; }
+        [Column(Name="feed_item_id", IsPrimaryKey=true)] public int feedItemID { get; set; }
+        [Column(Name="title")] public string title { get; set; }
+        [Column(Name="item_uri")] public string itemURI { get; set; }
+        [Column(Name="comments")] public string comments { get; set; }
+        [Column(Name="published_datetime")] public DateTime publishedDateTime { get; set; }
+        [Column(Name="creator")] public string creator { get; set; }
+        [Column(Name="description")] public string description { get; set; }
+        [Column(Name="content")] public string content { get; set; }
+        [Column(Name="last_download_datetime")] public DateTime lastDownloadDateTime { get; set; }
+    }
+}
