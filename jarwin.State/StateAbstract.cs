1d123198f327fad83d97cc6b0fb1f78acf340115
James Corbould <james.bron@hotmail.com>
2014-09-09 17:00:48 +1200

    Added State classes and Enterprise Library components.

---
+++ jarwin.State/StateAbstract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jarwin.State
+{
+    public abstract class StateAbstract
+    {
+        public string description { get; set; }
+    }
+}
---
+++ jarwin.State/StateRefreshRequired.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jarwin.State
+{
+    public class StateRefreshRequired : StateAbstract
+    {
+        public StateRefreshRequired()
+        {
+            description = "Refresh required...";
+        }
+    }
+}
